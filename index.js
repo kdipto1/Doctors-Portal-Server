ecfb33cd7049de2c66d0715a250c0c8d5ecdf77d
kdipto1 <kdipto@outlook.com>
2022-05-15 19:47:05 +0600

    api for getting users with admin role

--- index.js
+++ index.js
@@ -52,15 +52,29 @@ async function run() {
       const users = await userCollection.find().toArray();
       res.send(users);
     })
-    //admin
+    //admin get API
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin"
+      res.send({admin: isAdmin})
+    })
+    //admin make api
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: {role: "admin"},
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: {role: "admin"},
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else {
+        res.status(403).send({ message: "Forbidden"})
+      }
     });
     // user put
     app.put("/user/:email", async (req, res) => {
